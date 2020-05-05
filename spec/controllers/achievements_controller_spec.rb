ec78fb4e9d71361e0577ab53c9422df9f8c6340b
Carlos Anriquez <canriquez@gmail.com>
2020-05-05 08:20:22 -0300

    finished achievement show and create actions

--- spec/controllers/achievements_controller_spec.rb
+++ spec/controllers/achievements_controller_spec.rb
@@ -29,20 +29,40 @@ describe AchievementsController do
   end
 
   describe 'POST create' do
-    # let!(:achievement) { FactoryBot.build(:public_achievement) }
-    # FactoryBot.find_definitions
-    it 'redirects to achievement#show' do
-      attrs = FactoryBot.attributes_for(:public_achievement)
-      puts "here the create hash: "
-      p attrs
-      # post :create, params: { achievement: FactoryBot.find_definitions(:public_achievement) }
-      # post :create, params: { achievement: attrs }
-      post :create, params: { achievement: FactoryBot.attributes_for(:public_achievement) }
+    context "valid data" do
+        let(:valid_data) { FactoryBot.attributes_for(:public_achievement) }
+        # let!(:achievement) { FactoryBot.build(:public_achievement) }
+        # FactoryBot.find_definitions
+        it 'redirects to achievement#show' do
+        attrs = FactoryBot.attributes_for(:public_achievement)
+        puts "here the create hash: "
+        p attrs
+        # post :create, params: { achievement: FactoryBot.find_definitions(:public_achievement) }
+        # post :create, params: { achievement: attrs }
+        post :create, params: { achievement: valid_data }
 
-      expect(response).to redirect_to(achievement_path(assigns[:achievement]))
+        expect(response).to redirect_to(achievement_path(assigns[:achievement]))
+        end
+
+        it 'creates new achievement in database' do
+            expect {
+                post :create, params: { achievement: valid_data }
+            }.to change(Achievement, :count).by(1)
+        end
     end
 
-    it 'creates new achievement in database'
+    context "invalid data" do
+        let(:invalid_data) {  FactoryBot.attributes_for(:public_achievement, title: '') }
+        it "renders :new template" do
+            post :create, params: { achievement: invalid_data }
+            expect(response).to render_template(:new)
+        end
+        it "doesn't create new achievement in the database" do
+        expect {
+            post :create, params: { achievement: invalid_data }
+        }.not_to change(Achievement, :count)
+        end
+    end
   end
 end
 
